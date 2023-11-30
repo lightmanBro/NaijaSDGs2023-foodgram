16c7bf1be0902089f008bcf3efeadbefa70e98c8
LightmanBro <omotosodavidromeo@gmail.com>
2023-11-30 11:35:13 +0100

    worked on the post button

--- src/public/script/index.js
+++ src/public/script/index.js
@@ -2,15 +2,11 @@ const newPost = document.querySelector('#new-post');
 const addPostBtn = document.querySelector('.add');
 
 addPostBtn.addEventListener('click',(e)=>{
-    // addPostBtn.style.display = 'none';
     newPost.classList.remove('hidden');
-    console.log('button clicked')
-    console.log(newPost)
+    addPostBtn.style.display = 'none'
 })
 
 document.querySelector('.container-body').addEventListener('click',()=>{
-    // document.querySelector('.new-post').style.display = 'none';
-    // newPost.style.display = 'none';
     newPost.classList.add('hidden');
-    console.log('body clicked')
-})
+    addPostBtn.style.display = 'block'
+}) 
