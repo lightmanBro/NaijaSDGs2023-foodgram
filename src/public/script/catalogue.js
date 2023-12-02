bc85fe76f5f2b3dbfb621afa192e81c5e5ffc4f3
LightmanBro <omotosodavidromeo@gmail.com>
2023-12-02 10:15:35 +0100

    completed the ui of the order catalogue page

--- src/public/script/catalogue.js
+++ src/public/script/catalogue.js
@@ -1,35 +1,56 @@
-
+const newOrderBtn = document.querySelector('#new');
 const daily = document.querySelector('#daily')
 const weekly = document.querySelector('#weekly');
 const past = document.querySelector('#past');
+const newOrderContainer = document.querySelector('.new-order')
 const dailyOrders = document.querySelector('.normal-orders')
 const scheduleOrders = document.querySelector('.schedule-orders');
 const historyContainer = document.querySelector('.right-container');
 const historyContainerHead = document.querySelector('.right-container-heading');
+
 scheduleOrders.style.display = 'none'
+dailyOrders.style.display = 'none';
+
+
+newOrderBtn.addEventListener('click',()=>{
+    daily.classList.remove('active');
+    weekly.classList.remove('active');
+    past.classList.remove('active');
+    newOrderBtn.classList.add('active');
+    dailyOrders.style.display = 'none'
+    scheduleOrders.style.display = 'none'
+    historyContainer.style.display = 'none';
+    newOrderContainer.style.display = 'block'
+})
 
 weekly.addEventListener('click',(e)=>{
+    newOrderBtn.classList.remove('active');
     daily.classList.remove('active');
     weekly.classList.add('active');
     past.classList.remove('active');
     dailyOrders.style.display = 'none'
-    scheduleOrders.style.display = 'block'
     historyContainer.style.display = 'none';
+    newOrderContainer.style.display = 'none'
+    scheduleOrders.style.display = 'block'
 })
 
 daily.addEventListener('click',()=>{
+    newOrderBtn.classList.remove('active');
     daily.classList.add('active');
     weekly.classList.remove('active');
     past.classList.remove('active');
     scheduleOrders.style.display = 'none'
-    dailyOrders.style.display = 'block'
     historyContainer.style.display = 'none';
+    newOrderContainer.style.display = 'none'
+    dailyOrders.style.display = 'block';
 })
 
 past.addEventListener('click',()=>{
+    newOrderBtn.classList.remove('active');
     past.classList.add('active');
     weekly.classList.remove('active');
     daily.classList.remove('active');
+    newOrderContainer.style.display = 'none'
     dailyOrders.style.display = 'none'
     scheduleOrders.style.display = 'none'
     historyContainer.style.display = 'block';
