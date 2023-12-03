fa2ac717c6fc90aa3cd046010d7097bb1de2fd3c
LightmanBro <omotosodavidromeo@gmail.com>
2023-12-04 00:33:16 +0100

    added an ai script for the ai page test

---
+++ src/public/script/ai.js
@@ -0,0 +1,98 @@
+class FoodOrderSystem {
+    constructor(vendors, userAllergens) {
+        this.vendors = vendors;
+        this.orders = [];
+        this.userAllergens = userAllergens;
+    }
+
+    displayAvailableMeals() {
+        this.vendors.forEach(vendor => {
+            console.log(`\nVendor: ${vendor.name}`);
+            vendor.meals.forEach(meal => {
+                console.log(`${meal.name} - $${meal.price}`);
+            });
+        });
+    }
+
+    //Will pass vendor id instead of vendor index
+    placeOrder(vendorIndex, mealIndex) {
+        const vendor = this.vendors[vendorIndex];
+        const selectedMeal = vendor.meals[mealIndex];
+
+        if (selectedMeal.allergens.some(allergen => this.userAllergens.includes(allergen))) {
+            console.log("Warning: This meal contains allergens. Consider choosing a different meal.");
+            return;
+        }
+
+        this.orders.push({
+            vendor: vendor.name,
+            meal: selectedMeal.name,
+            price: selectedMeal.price,
+            foodClasses: selectedMeal.foodClasses
+        });
+
+        console.log(`\nOrder placed: ${selectedMeal.name} from ${vendor.name}`);
+    }
+
+    planMealsForWeek(budget, days) {
+        const dailyBudget = budget / days;
+        const dailyOrders = [];
+
+        for (let day = 1; day <= days; day++) {
+            let remainingBudget = dailyBudget;
+            const dailyMeals = [];
+
+            this.vendors.forEach(vendor => {
+                vendor.meals.forEach(meal => {
+                    if (
+                        meal.price <= remainingBudget
+                        && meal.foodClasses.includes('protein')
+                        && meal.foodClasses.includes('carbohydrates')
+                        && !meal.allergens.some(allergen => this.userAllergens.includes(allergen))
+                    ) {
+                        dailyMeals.push({
+                            vendor: vendor.name,
+                            meal: meal.name,
+                            price: meal.price
+                        });
+                        remainingBudget -= meal.price;
+                    }
+                });
+            });
+
+            dailyOrders.push(dailyMeals);
+        }
+
+        return dailyOrders;
+    }
+}
+
+// Example usage
+//fetching the vendor list from the database and their foods data too.
+const vendorsList = [
+    { name: 'Vendor A', meals: [{ name: 'Jollof Rice', price: 5, foodClasses: ['carbohydrates'], allergens: ['nuts', 'shellfish'] }] },
+    {
+        name: 'Mama Bee Kitchen', meals: [{ name: 'Grilled Chicken', price: 8, foodClasses: ['protein', 'carbohydrates'], allergens: ['dairy'] },
+        { name: 'Grilled Chicken', price: 8, foodClasses: ['protein', 'carbohydrates'], allergens: ['dairy'] }]
+    },
+    {
+        name: 'Vendor B', meals: [{ name: 'Vegetable Soup', price: 4, foodClasses: ['vegetables'], allergens: ['gluten'] },
+        { name: 'Fish Stew', price: 7, foodClasses: ['protein'], allergens: ['dairy'] }]
+    }
+];
+
+//The user allergies list
+const userAllergens = [];
+
+const foodOrderSystem = new FoodOrderSystem(vendorsList, userAllergens);
+foodOrderSystem.displayAvailableMeals();
+
+// User clicks "Accept" button
+foodOrderSystem.placeOrder(0, 2);  // Example: Ordering Grilled Chicken from Vendor A
+
+// Planning meals for a week
+const weeklyOrders = foodOrderSystem.planMealsForWeek(20);
+console.log("\nWeekly Meal Plan:");
+weeklyOrders.forEach((dailyMeals, day) => {
+    console.log(`Day ${day + 1}: ${dailyMeals.map(meal => meal.meal + ' from ' + meal.vendor).join(', ')}`);
+});
